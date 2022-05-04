39456e8e803c3ce6562f9e65b8671b75096868dc
Rafael Paes <rfaelpaes@gmail.com>
2022-05-04 01:35:51 -0300

    refactor: add new color and border radius and config plugins form and scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,17 @@ module.exports = {
     extend: {
       colors: {
         brand: {
+          300: '#996DFF',
           500: '#8257e6'
         }
+      },
+      borderRadius: {
+        md: '4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar')
+  ],
 }
