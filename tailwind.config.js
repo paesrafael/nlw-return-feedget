25e0b02c04d38bb73fff3a215e181d8baf3e666f
Rafael Paes <rfaelpaes@gmail.com>
2022-05-03 21:19:02 -0300

    build: install new packages of UI and Icon

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          500: '#8257e6'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
