9b3a7573a9fb1c221e5de3a32b81398d2cc399ed
Rafael Paes <rfaelpaes@gmail.com>
2022-05-04 10:10:17 -0300

    refactor: add a new WidgetForm component in the main Widget

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -2,10 +2,14 @@ import { Popover } from '@headlessui/react'
 
 import { ChatTeardropDots } from 'phosphor-react'
 
+import WidgetForm from './components/WidgetForm'
+
 const Widget = () => {
   return (
-    <Popover className="absolute bottom-4 right-4">
-      <Popover.Panel>Hello World</Popover.Panel>
+    <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
 
       <Popover.Button
         className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
