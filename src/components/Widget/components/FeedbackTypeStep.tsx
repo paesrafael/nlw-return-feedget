6d7855a528b40c582e096b7fc87a0c0a7446f37d
Rafael Paes <rfaelpaes@gmail.com>
2022-05-05 12:01:41 -0300

    refactor: add isLoading and show icon before feedback types are loaded

--- src/components/Widget/components/FeedbackTypeStep.tsx
+++ src/components/Widget/components/FeedbackTypeStep.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
+
 import CloseButton from '../../CloseButton'
+import Loading from '../../Loading'
 
 import { FeedbackTypeStepProps } from '../interfaces'
 
 import { FeedbackType, feedbackTypes } from '../../../utils/feedbackTypes'
 
 const FeedbackTypeStep = ({ onFeedbackTypeChanged }: FeedbackTypeStepProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+
+  setTimeout(() => {
+    setIsLoading(false)
+  }, 730)
+
   return (
     <>
       <header>
@@ -14,18 +23,24 @@ const FeedbackTypeStep = ({ onFeedbackTypeChanged }: FeedbackTypeStepProps) => {
       </header>
 
       <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, value]) =>
-          <button
-            key={key}
-            type="button"
-            onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
-            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent
-              hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-          >
-            <img src={value.image.source} alt={value.image.alt} />
+        {isLoading ? (
+          <div className="w-full flex items-center justify-center">
+            <Loading size="4rem" heart />
+          </div>
+        ) : (
+          Object.entries(feedbackTypes).map(([key, value]) =>
+            <button
+              key={key}
+              type="button"
+              onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent
+                hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
 
-            <span>{value.title}</span>
-          </button>
+              <span>{value.title}</span>
+            </button>
+          )
         )}
       </div>
     </>
