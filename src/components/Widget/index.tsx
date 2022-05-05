2fec9f8c85fb18254eaa545aef92dbfa88afc49f
Rafael Paes <rfaelpaes@gmail.com>
2022-05-04 23:07:34 -0300

    fix: widget position changed to fixed

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -6,7 +6,7 @@ import WidgetForm from './components/WidgetForm'
 
 const Widget = () => {
   return (
-    <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
+    <Popover className="fixed bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
       <Popover.Panel>
         <WidgetForm />
       </Popover.Panel>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Widget from '../components/Widget'
 
 const Home = () => {
   return (
-    <div className="relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">     
+    <div className="max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">     
       <header>
         <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight
           text-center dark:text-white"
